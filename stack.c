ac3ae337c4955db76814d32db8dc07e9dfc652cd
Aravind Vaddi <aravindvaddi.github@gmail.com>
2017-07-06 12:57:21 +0530

    Added the following - function to print stack - function to return length of stack - function to initialize stack - updated header file with definitions and added more definitions as well

--- stack.c
+++ stack.c
@@ -1,4 +1,35 @@
 #include <stdlib.h>
 #include "stack.h"
 
+/* function to return the length of a stack */
 
+int s_length (stack *s)
+{
+	int i;
+	node *temp = s->head;
+
+	for(i = 0; temp; i++, temp = temp->next);
+
+	return i;
+}
+
+/* function to print all elements of a stack */
+
+void s_print (stack *s)
+{
+	node *temp = s->head;
+	while(temp)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+
+/* function to allocate memory to a given reference to a stack pointer */
+
+void s_create (stack **s)
+{
+	*s = malloc(sizeof **s);
+	s->head = NULL;
+}
--- stack.h
+++ stack.h
@@ -18,11 +18,17 @@ typedef struct stack stack;
 /* stack debugging + helper functions */
 
 void s_print(stack *s);
+int s_length(stack *s);
 
 /* stack core functions */
 int s_empty (stack *s);
 int s_top(stack *s);
 void s_pop(stack *s);
 void s_push (stack *s, int data);
+
+/* stack generate and destroy functions */
+
+void s_create (stack **s);
+void s_destroy (stack **s);
 
 #endif
