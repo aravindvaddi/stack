6c4046dc80161130893ee27e13bfe6f77c87919d
Aravind Vaddi <aravindvaddi.github@gmail.com>
2017-07-06 12:47:24 +0530

    Initialized header file with function definitions

---
+++ stack.h
@@ -0,0 +1,28 @@
+#ifndef STACK_H
+#define STACK_H
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+struct stack
+{
+	struct node *head;
+};
+
+typedef struct node node;
+typedef struct stack stack;
+
+/* stack debugging + helper functions */
+
+void s_print(stack *s);
+
+/* stack core functions */
+int s_empty (stack *s);
+int s_top(stack *s);
+void s_pop(stack *s);
+void s_push (stack *s, int data);
+
+#endif
